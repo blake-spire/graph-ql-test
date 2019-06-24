1406742e53d24b02bf08c1c7917c1a85bd05d365
blake <blake.netzeband@spiredigital.com>
2019-06-24 14:04:30 -0600

    update casing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { ApolloServer } = require("apollo-server");
 const mongoose = require("mongoose");
 
 const { MONGODB } = require("./config");
-const typeDefs = require("./graphql/typedefs");
+const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 
 const server = new ApolloServer({ typeDefs, resolvers });

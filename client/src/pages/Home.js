841f58eb8c733cf025c89a30b11d93afa716a834
blake <blake.netzeband@spiredigital.com>
2019-07-11 08:28:57 -0600

    added post card component and basic gql setup

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,60 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import { Grid } from "semantic-ui-react";
+
+import PostCard from "../components/PostCard";
 
 const Home = () => {
+  const {
+    loading,
+    data: { getPosts: posts },
+  } = useQuery(FETCH_POST_QUERY);
+
   return (
-    <section>
-      <h1>Home page</h1>
-    </section>
+    <Grid columns={3}>
+      <Grid.Row>
+        <h1>Recent Posts</h1>
+      </Grid.Row>
+
+      <Grid.Row>
+        {loading ? (
+          <h1>Loading posts...</h1>
+        ) : (
+          posts &&
+          posts.map(post => {
+            return (
+              <Grid.Column key={post.id} style={{ marginBottom: "1em" }}>
+                <PostCard post={post} />
+              </Grid.Column>
+            );
+          })
+        )}
+      </Grid.Row>
+    </Grid>
   );
 };
+
+const FETCH_POST_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`;
 
 export default Home;
